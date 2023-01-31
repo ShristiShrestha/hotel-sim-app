1ddb3cc403c5a81bea0edc9d21a98282a4d294f1
ShristiShrestha <sthashristi777@gmail.com>
2023-01-31 14:50:35 -0600

    fix rebase issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "../redux/store";
-import '@/styles/globals.css';
-import '@/styles/toastr.css';
+import "../styles/globals.css";
+import "../styles/toastr.css";
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
     return (
         <Provider store={store}>
             <Component {...pageProps} />
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,11 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-<<<<<<< HEAD
-export default (req, res) => {
-    res.status(200).json({ name: "John Doe" });
-};
-=======
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
->>>>>>> 131aa15 (Initial commit)
